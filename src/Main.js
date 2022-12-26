7f0011588cca74f449a8765ce861882045c537ce
TheaKevin <theakevin01@gmail.com>
2022-12-26 12:03:50 +0700

    nambah routes ke auto checkout

--- src/Main.js
+++ src/Main.js
@@ -121,6 +121,7 @@ function Main() {
                 <Route path="/shopping-cart" element={ <ShoppingCart/> }/>
                 <Route path={"/DetailProduk/"+idProduk} element={ <DetailProduk idProduk={idProduk} /> }/>
                 <Route path="/checkout" element={ <Checkout/> }/>
+                <Route path="/AutoCheckout" element={ <AutoCheckout /> }/>
                 <Route path="/pilih-pembayaran" element={ <PilihPembayaran setImg={setImg} setNoRekening={setNoRekening}/> }/>
                 <Route path={"/pembayaran/"+img+"/"+noRekening} element={ <Pembayaran img={img} noRekening={noRekening}/> }/>
                 <Route path="/request-product" element={ <RequestProduct/> }/>
